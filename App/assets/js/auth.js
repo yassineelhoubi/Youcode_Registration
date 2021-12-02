c489064bd701fb2f6e46903c72aa27b014606339
yassineelhoubi <elhoubiyassine@gmail.com>
2021-12-02 10:54:19 +0100

    change structure of registration process

--- App/assets/js/auth.js
+++ App/assets/js/auth.js
@@ -44,6 +44,7 @@ class Candidate extends User {
     }
 }
 
+/* submit registration form */
 const form = document.querySelector('.register')
 form.addEventListener('submit', (e) => {
     e.preventDefault();
@@ -52,35 +53,63 @@ form.addEventListener('submit', (e) => {
     const age = form.age.value;
     const lName = form.lName.value;
     const fName = form.fName.value;
-    const UserLogin = `${Math.random()
-        .toString(36)
-        .substr(3, 4)}`;
-    const password = `${Math.random()
-        .toString(36)
-        .substr(2, 6)}`;
-    test = new Candidate(cin, age, lName, fName, email, UserLogin, password)
-    obj = test.register()
-    Swal.fire({
-        title: 'Do you want to save your info?',
-        html: `Your Login :${UserLogin}<br>Your Password:${password}`,
-        showDenyButton: true,
-        showCancelButton: true,
-        confirmButtonText: 'Save',
-        denyButtonText: `Don't save`,
-    }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
-        if (result.isConfirmed) {
-            fetch("http://localhost:3000/users/", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                },
-                body: JSON.stringify(obj),
-            });
-            Swal.fire('Saved!', '', 'success')
-            location.href = 'login.html';
-        } else if (result.isDenied) {
-            Swal.fire('Infos are not saved', '', 'info')
-        }
-    })
+    if (cin == "" || email == "" || age == "" || lName == "" || fName == "") {
+
+        Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'Incomplete information!',
+
+        })
+
+    } else {
+        const UserLogin = `${Math.random()
+            .toString(36)
+            .substr(3, 4)}`;
+        const password = `${Math.random()
+            .toString(36)
+            .substr(2, 6)}`;
+        newCandidate = new Candidate(cin, age, lName, fName, email, UserLogin, password)
+        obj = newCandidate.register()
+        Swal.fire({
+            title: 'Do you want to save your info?',
+            html: `Your Login :${UserLogin}<br>Your Password:${password}`,
+            showDenyButton: true,
+            showCancelButton: true,
+            confirmButtonText: 'Save',
+            denyButtonText: `Don't save`,
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+
+            if (result.isConfirmed) {
+                fetch("http://localhost:3000/users/", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(obj),
+                }).then(
+                    //register successfuly
+                    Swal.fire({
+                        title: 'Please Remember Your Login?',
+                        html: `Your Login : <strong>${UserLogin}</strong><br>Your Password: <strong>${password}</strong>`,
+                        icon: 'warning',
+                        showCancelButton: true,
+                        confirmButtonColor: '#3085d6',
+                        cancelButtonColor: '#d33',
+                        confirmButtonText: 'I saved it!'
+                      }).then((result) => {
+                        if (result.isConfirmed) {
+                            Swal.fire('Saved!', ``, 'success')
+                            location.href = 'login.html';
+                        }
+                      })
+                );
+
+            } else if (result.isDenied) {
+                Swal.fire('Infos are not saved', '', 'info')
+            }
+        })
+    }
+
 })
