e34648c155b25dae45bd9663c79821e11150a3f9
yassineelhoubi <elhoubiyassine@gmail.com>
2021-12-03 20:56:09 +0100

    add new class and add some update

---
+++ App/assets/js/Result.js
@@ -0,0 +1,17 @@
+export class Result {
+    constructor(idUser, onlineTest, seriousGame, motivationTest, administrationTest1, administrationTest2, administrationTest3, technicalTest) {
+        this.idUser = idUser;
+        this.onlineTest = onlineTest;
+        this.seriousGame = seriousGame;
+        this.motivationTest = motivationTest;
+        this.administrationTest1 = administrationTest1;
+        this.administrationTest2 = administrationTest2;
+        this.administrationTest3 = administrationTest3;
+        this.technicalTest = technicalTest;
+
+    }
+
+    add () {
+        
+    }
+}
